87982a42f2e25d4f25ce6353c4101fb1ce686f02
NeerajKumar-DevOp <neeraj3bk@gmail.com>
2026-03-23 17:49:18 +0530

    First commit - Terraform aws-vpc module

---
+++ output.tf
@@ -0,0 +1,12 @@
+output "vpc_id" {
+    value = aws_vpc.main.id
+}
+output "public_subnet_ids" {
+    value = aws+subnet.public[*].id
+}
+output "private_subnet_ids" {
+    value = aws+subnet.private[*].id
+}
+output "database_subnet_ids" {
+    value = aws+subnet.database[*].id
+}
---
+++ readme.md
@@ -0,0 +1,6 @@
+# AWS VPC
+
+This module creates VPC.
+
+### Inputs
+please provide all inputs.
